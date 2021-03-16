d75e7e3230991b5887069dfc7a6c4345963e687d
TaeHongGil <66548142+TaeHongGil@users.noreply.github.com>
2021-03-17 01:35:59 +0900

    Delete 1.py

--- python/1.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-def solution(numbers):
-    return sorted(list(set(list(map(lambda x: x[0]+x[1],list(itertools.combinations(numbers,2)))))))
-
-n = [2,1,3,4,1]
-
-print(solution(n))
